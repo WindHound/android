11f33185283b2cca647aede460439e5857fcf366
Will Smith <ws16062@bristol.ac.uk>
2018-02-07 20:52:16 +0000

    fixed login activity interface issues
    Stopped the on-screen keyboard automatically popping upon launching the application and added a cursor to the editText components.

--- app/src/main/java/windshift/windhound/LoginActivity.java
+++ app/src/main/java/windshift/windhound/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.EventLog;
 import android.view.View;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Stops the on-screen keyboard automatically popping up
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.
+                SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     public void login(View view) {
